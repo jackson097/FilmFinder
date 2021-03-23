7a201b64a341deda05b6eb12a2924ea45481d91b
Kumiko Randle <kcrandle98@hotmail.com>
2021-03-23 15:38:58 -0400

    Created superuser & made view for movie
    Created superuser for self and made view for movie to be accessed by search page. Deleted home_page.html

--- src/Movies/views.py
+++ src/Movies/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from Movies.models import Movie
 
 # Create your views here.
+def home(request): 
+    movies = Movie.objects.all() 
+    return render(request,'main/index.html',{"movies":movies}) 
