eff48b7f4ebf72c38974c45ba48d7f672ae2cd00
Kumiko Randle <kcrandle98@hotmail.com>
2021-03-23 16:05:10 -0400

    Changes

--- src/filmFinder/views.py
+++ src/filmFinder/views.py
@@ -2,12 +2,23 @@ from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
-from Movies.models import Movie
+from Movies.models import Movie, MovieGenre
+from Genres.models import Genre
 
 def home_page(request):
     movie = Movie.objects.all()
+    genre = Genre.objects.all()
     context = {
         "title":"Film Finder",
-        "movies": movie
+        "movies": movie,
+        "genre":genre
     }
+    if request.method == "POST":
+        #pass thru info here
+        return redirect("result")
     return render(request, "search.html", context)
+
+def results_page(request):
+
+    return render(request, "results.html", {})
+ 
