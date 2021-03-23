f015724d8c7bb6305168b9166a53491b0cd406f2
Kumiko Randle <kcrandle98@hotmail.com>
2021-03-23 15:57:55 -0400

    Added context variable in movie views

--- src/Movies/views.py
+++ src/Movies/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from Movies.models import Movie
+from Genres.models import Genre
 
 # Create your views here.
 def home(request): 
     movies = Movie.objects.all() 
-    return render(request,'search.html',{"movies":movies}) 
+    genres = Genre.objects.all()
+    
+    context = {
+        "movies":movies,
+        "genres":genres
+    }
+
+    return render(request,'search.html', context) 
