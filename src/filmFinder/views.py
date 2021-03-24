16ec6ef41e98f359247382d3aa136597d6853f2b
Ashaab Khan <ashaabkhan833@gmail.com>
2021-03-23 23:47:09 -0400

    idk its asking me to commit

--- src/filmFinder/views.py
+++ src/filmFinder/views.py
@@ -6,19 +6,32 @@ from Movies.models import Movie, MovieGenre
 from Genres.models import Genre
 
 def home_page(request):
-    movie = MovieGenre.objects.all()
-    genre = Genre.objects.all()
+    movies = Movie.objects.all() 
+    genres = Genre.objects.all()
+    movies_genres = MovieGenre.objects.all()
+    movie_genre_list = []
+
+    for movie in movies:
+        # Takes first genre for a given movie (movies can have more than one genre)
+        movie_genre = movies_genres.filter(movie_id=movie.movie_id).first()
+
+        if movie_genre is not None:
+            genre = genres.get(genre_id=movie_genre.genre_id.genre_id).genre_title
+        else: # If movie has no genre
+            genre = "No genre"
+
+        movie_genre_list.append([movie, genre]) # Format: [Movie Object, string]
+    
     context = {
         "title":"Film Finder",
-        "movies": movie,
-        "genre":genre
+        "movies": movie_genre_list
     }
     if request.method == "POST":
         #pass thru info here
         return redirect("result")
+    
     return render(request, "search.html", context)
 
 def results_page(request):
-
     return render(request, "results.html", {})
  
