fc0532129b517cbe4e3792c5a9b14c761988df44
Kumiko Randle <kcrandle98@hotmail.com>
2021-03-26 00:09:49 -0400

    Styled login box and added some validation

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth import get_user_model
+from django.contrib import messages
 
 from .forms import LoginForm, RegisterForm
 
@@ -19,7 +20,9 @@ def login_page(request):
             login(request, user)
             return redirect('index')
         else:
-            print("error")    
+            messages.error(request, "Username or password is incorrect")
+            return redirect('login')  
+    
     return render(request, "accounts/login.html", context)
 
 User = get_user_model()
