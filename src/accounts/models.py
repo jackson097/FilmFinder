44aa1fa31e6667982a0f3c9f9abbd6ba51594c09
Ashaab Khan <ashaabkhan833@gmail.com>
2021-03-27 21:30:40 -0400

    fixed profile pic issue & added tweaked top movies

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager)
+import random
+import os
+
+def get_filename_ext(filepath):
+	base_name = os.path.basename(filepath)
+	name, ext = os.path.splitext(base_name)
+	return name, ext
 
 def upload_image_path(instance, filename):
-    new_filename = random.randint(1,3910209312)
-    name, ext = get_filename_ext(filename)
-    final_filename = '{new_filename}{ext}'.format(new_filename=new_filename, ext=ext)
-    return "users/{new_filename}/{final_filename}".format(new_filename=new_filename,final_filename=final_filename)
+	new_filename = random.randint(1,3910209312)
+	name, ext = get_filename_ext(filename)
+	final_filename = '{new_filename}{ext}'.format(new_filename=new_filename, ext=ext)
+	return "products/{new_filename}/{final_filename}".format(new_filename=new_filename,final_filename=final_filename)
 
 class UserManager(BaseUserManager):
     def create_user(self, email, full_name=None, password=None, is_active=True, is_staff=False, is_admin=False):
